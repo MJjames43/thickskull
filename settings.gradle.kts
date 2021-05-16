bbd3d70d6278dc636d411492a5b489b895b59dfb
Mark Guidry <81898757+MJjames43@users.noreply.github.com>
2021-05-16 07:09:17 -0500

    Initialize gradle-intellij-plugin-1.0

---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+rootProject.name = "IntelliJ Platform Plugin Template"
+
+pluginManagement {
+    repositories {
+        maven("https://oss.sonatype.org/content/repositories/snapshots/")
+        gradlePluginPortal()
+    }
+}
